e8f6e4bdbf4fca9faa89ad8199dc45468fec8fc0
LamSut <lamsut0007@gmail.com>
2025-07-30 08:24:33 +0700

    nil hil

--- projects/aws/s3-static/variables.tf
+++ projects/aws/s3-static/variables.tf
@@ -1,9 +1,9 @@
 variable "static_policy" {
   type    = string
-  default = "./conf/s3_static_policy.json"
+  default = "./env/dev/s3_static_policy.json"
 }
 
 variable "asset_path" {
   type    = string
-  default = "./conf/limweb"
+  default = "./env/dev/limweb"
 }
