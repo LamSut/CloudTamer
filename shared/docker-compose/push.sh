010c59aadd773803900c4bbce615b581b7779aca
LamSut <lamsut0007@gmail.com>
2025-08-25 11:26:23 +0700

    clean docker script

--- shared/docker-compose/push.sh
+++ shared/docker-compose/push.sh
@@ -4,7 +4,7 @@ set -e
 # Clone the repository if not already cloned
 echo "Checking if the repository is already cloned..."
 if [[ ! -d "containerize-full-stack-app-MERN-with-docker-compose" ]]; then
-    git clone https://github.com/bjnandi/containerize-full-stack-app-MERN-with-docker-compose.git repo
+    git clone https://github.com/LamSut/PizzaGout.git repo
     echo "Repository cloned successfully."
 else
     echo "Repository already cloned."
